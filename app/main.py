f8fe20cac8321150602445f49ed95cba0576e9b9
RemiVander <rm.vanderperre@gmail.com>
2025-06-04 15:10:11 +0200

    add admin

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from sqlmodel import SQLModel, create_engine, Session, select
 from models.teacher import Teacher
+from models.admin import Admin
 from datetime import date
 from models.user import User
 engine = create_engine("sqlite:///test.db", echo=True)
@@ -7,20 +8,33 @@ engine = create_engine("sqlite:///test.db", echo=True)
 
 SQLModel.metadata.create_all(engine)
 
-teacher = Teacher(
-    name="Alice",
-    surname="Durand",
-    email="alice@example.com",
-    is_active=True,
-    role="teacher",
-    speciality="Data Science",
-    hiring_date=date(2021, 5, 15),
-    hours_rate=60.0,
-    bio="Experte en machine learning."
-)
+
 
 with Session(engine) as session:
+    teacher = Teacher(
+        name="Alice",
+        surname="Durand",
+        email="alice@example.com",
+        is_active=True,
+        role="teacher",
+        speciality="Data Science",
+        hiring_date=date(2021, 5, 15),
+        hours_rate=60.0,
+        bio="Experte en machine learning."
+    )
+
+    admin = Admin(
+        name="John",
+        surname="Doe",
+        email="jd@example.com",
+        is_active=True,
+        role="admin",
+        hiring_date=date(2018, 5, 15)
+    )
+
+
     session.add(teacher)
+    session.add(admin)
     session.commit()
 
     result = session.exec(select(Teacher)).all()

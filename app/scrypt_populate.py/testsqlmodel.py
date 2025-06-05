28d13acc1677b383d2125b68bfe34b20ff87d87d
stephane <Vous@exemple.com>
2025-06-05 11:51:19 +0200

    add scrypt populate bdd. teacher and room ok. Student nok

--- app/scrypt_populate.py/testsqlmodel.py
+++ app/scrypt_populate.py/testsqlmodel.py
@@ -1,16 +1,24 @@
+import sys, os
+sys.path.append(os.getcwd())
+
 from sqlmodel import Field, Session, SQLModel, create_engine, select, delete, func
 from faker import Faker
-from models.teacher import Teacher
-from models.admin import Admin
-from models.room import Room
-from main import SQLModel,engine, Session, select
-from datetime import date
+import random
 
+from app.models.teacher import Teacher
+from app.models.user import User
+from app.models.room import Room
+from app.models.student import Student
+from app.models.admin import Admin
+
+
+from datetime import date
+engine = create_engine("sqlite:///centre_de_formation.db", echo=True)
+SQLModel.metadata.create_all(engine)
 
 fake = Faker()
 
-
-# Fonction pour générer des données fictives et les insérer dans la base de données
+# Fonction pour générer des données fictives teacher et les insérer dans la base de données
 def populate_table_teacher(num_teacher: int = 10):
     with Session(engine) as session:
         for _ in range(num_teacher):
@@ -28,9 +36,63 @@ def populate_table_teacher(num_teacher: int = 10):
             )
 
             session.add(teacher)
+    
+        session.commit()
+
+#  Fonction pour générer des données fictives teacher et les insérer dans la base de données
+# def populate_table_user(num_user: int = 10):
+#     with Session(engine) as session:
+#         for _ in range(num_user):
+#             user = User(
+#                 name=fake.unique.first_name(),
+#                 surname=fake.unique.last_name(),
+#                 email=fake.email(),
+#                 creation_date= fake.date(),
+#                 is_active=True,
+#                 role="user"             
+#             )
+
+#             session.add(user)
+    
+#         session.commit()
+
+# # Fonction pour générer des salles fictives et les insérer dans la base de données
+def populate_table_room(num_room: int = 10):
+    with Session(engine) as session:
+        for _ in range(num_room):
+            room = Room(
+                name=fake.unique.first_name(),
+                capacite=fake.random_int(min=1, max=10),
+                localization=fake.street_address(),
+                is_active=True,
+                role="user"
+            )
+
+            session.add(room)
+    
+        session.commit()
+
+# # Fonction pour générer des étudiants fictifs et les insérer dans la base de données
+def populate_table_student(num_student: int = 1):
+    with Session(engine) as session:
+        for _ in range(num_student):
+            student = Student(
+                name=fake.unique.first_name(),
+                surname=fake.unique.last_name(),
+                birth_date=fake.date_of_birth(minimum_age=16, maximum_age=30),
+                level_degree=random.randint(1, 7),
+                telephone=random.randint(1, 100),
+                registration_date=date(2021, 5, 15),
+                diploma="Data engineer"
+            )
+
+            session.add(student)
+    
         session.commit()
 
 # Appeler la fonction pour peupler la table avec 10 héros fictifs
 populate_table_teacher(10)
-
+#populate_table_user(20)
+populate_table_room(6)
+populate_table_student(2)
 

1bf2d1c598015ca90f5e46d6fc1ba8dfee4ce5d3
elias <jean-pierre.elias@hotmail.fr>
2025-06-05 16:30:54 +0200

    feat: :sparkles: feature session

---
+++ app/models/class_session.py
@@ -0,0 +1,38 @@
+from sqlmodel import Field, SQLModel
+from datetime import datetime
+from sqlalchemy.sql import func
+from typing import Optional
+from pydantic import model_validator
+
+class ClassSession(SQLModel, table = True):
+    __tablename__ = "class_session"
+
+
+    id : int | None = Field(default=None, primary_key=True)
+    title: str = Field(max_length=100)
+    description : Optional[str] = None
+    start_date: datetime
+    end_date : datetime
+    #TO DO :max_capacity < room.capacity
+    max_capacity : int
+    statut_id : str = Field(default='OPEN', foreign_key='status.id')
+    #TO DO :requirement is a list
+    requirement_id : str | None = Field(default=None, foreign_key='requirement.id')
+    room_id : int = Field(foreign_key='room.id') 
+    teacher_id : int = Field(foreign_key='teacher.id') 
+
+    @model_validator(mode='after')
+    def check_dates(self):
+        if self.start_date >= self.end_date:
+            raise ValueError("start must be before end date")
+        return self
+    
+class Requirement(SQLModel, table = True):
+    __tablename__ = "requirement"
+    id : int | None = Field(default=None, primary_key=True)
+    name : str = Field(unique=True)
+
+class Status(SQLModel, table = True):
+    __tablename__ = "status"
+    id : int | None = Field(default=None, primary_key=True)
+    type : str = Field(unique=True)

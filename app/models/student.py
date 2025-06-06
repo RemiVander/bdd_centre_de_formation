c794b4e4bb04e1c915962362837e358032e98beb
elias <jean-pierre.elias@hotmail.fr>
2025-06-06 13:32:38 +0200

    feat: :sparkles: add model validator / change type class

--- app/models/student.py
+++ app/models/student.py
@@ -9,7 +9,7 @@ class Student(User, table=True):
     __tablename__ = "student"
 
     birth_date: date
-    level_degree: Optional[str] = None
-    telephone: Optional[str] = None
+    level_degree: Optional[int] = None
+    telephone: Optional[int] = None
     registration_date: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     diploma: Optional[str] = None

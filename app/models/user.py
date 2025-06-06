afa32951d7f44e70205c0d78fac9f15148875c9b
elias <jean-pierre.elias@hotmail.fr>
2025-06-06 14:10:57 +0200

    feat: :zap: Email STR pydantic

--- app/models/user.py
+++ app/models/user.py
@@ -1,13 +1,14 @@
 from sqlmodel import Field, SQLModel
 from datetime import datetime
 from sqlalchemy.sql import func
+from pydantic import EmailStr
 
 
 class User(SQLModel, table = False):
     id : int | None = Field(default=None, primary_key=True)
-    name: str
-    surname : str
-    email: str
+    name: str = Field(max_length=50)
+    surname : str = Field(max_length=50)
+    email: EmailStr
     creation_date : datetime = Field(default_factory=datetime.utcnow)
     is_active : bool
     role : str
